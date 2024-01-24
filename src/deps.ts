67f49389ea8f3ebe7b9d69aa272e5e7efe273d0a
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-01-24 22:50:35 +0300

    fix: add ^ to renovate-deps for patch-updating & remove // @renovate tag and add automerge

--- src/deps.ts
+++ src/deps.ts
@@ -1,58 +1,30 @@
 export const dependencies = {
-	// @renovate
 	elysia: "^0.8.9",
-	// @renovate
 	typescript: "^5.3.3",
-	// @renovate
 	"@types/bun": "^1.0.2",
-	// @renovate
 	"@biomejs/biome": "^1.5.2",
-	// @renovate
 	eslint: "^8.56.0",
-	// @renovate
 	"eslint-config-standard-with-typescript": "^43.0.1",
-	// @renovate
 	"eslint-plugin-promise": "^6.1.1",
-	// @renovate
 	"eslint-plugin-import": "^2.29.1",
-	// @renovate
 	"eslint-plugin-n": "^16.6.2",
-	// @renovate
 	"@typescript-eslint/eslint-plugin": "^6.19.0",
-	// @renovate
 	"eslint-plugin-drizzle": "^0.2.3",
-	// @renovate
 	prisma: "^5.8.1",
-	// @renovate
 	"drizzle-orm": "^0.29.3",
-	// @renovate
 	"drizzle-kit": "^0.20.13",
-	// @renovate
 	pg: "^8.11.3",
-	// @renovate
 	"@types/pg": "^8.10.9",
-	// @renovate
 	postgres: "^3.4.3",
-	// @renovate
 	mysql2: "^3.7.1",
-	// @renovate
 	husky: "^8.0.3",
-	// @renovate
 	"@elysiajs/bearer": "^0.8.0",
-	// @renovate
 	"@elysiajs/cors": "^0.8.0",
-	// @renovate
 	"@elysiajs/html": "^0.8.0",
-	// @renovate
 	"@kitajs/ts-html-plugin": "^1.3.3",
-	// @renovate
 	"@elysiajs/jwt": "^0.8.0",
-	// @renovate
 	"@elysiajs/server-timing": "^0.8.0",
-	// @renovate
 	"@elysiajs/static": "^0.8.1",
-	// @renovate
 	"@elysiajs/swagger": "^0.8.3",
-	// @renovate
 	"elysia-autoload": "^0.1.2",
 };
